09a735ce35130ced6bf5d06904bf0e9f9bda86c7
Suhanii1214 <suhani.12010153@lpu.in>
2024-02-08 05:17:20 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { router } from './routes.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
