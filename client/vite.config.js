b98f87eda301cc300c3c8ee358cae0ad35ae48fd
Suhanii1214 <suhani.12010153@lpu.in>
2024-02-08 05:02:19 +0530

    deployed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://filmbox-tt11.onrender.com",
         changeOrigin: true,
       },
     },
